a2bac9c5939a2030c72f20a3b5ee37fd124913b5
Pujan <pujan_sth@yahoo.com>
2025-10-23 21:34:39 +1100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     // reactStrictMode: true,
     // either omit `output`, or use 'standalone' (good for Docker/Node)
-    // output: 'standalone',
+    output: 'standalone',
     // output: 'export',
 
 };
