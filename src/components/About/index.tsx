e1cac6dbcd1e5380c27124aa01c069b080b8debe
Pujan Shrestha <pujan_sth@yahoo.com>
2025-10-26 12:09:12 +1100

    change git ignore and about page

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -7,7 +7,7 @@ const About = () => {
             <section className="dark:bg-darkmode pt-8 pb-24">
                 <div className="container mx-auto lg:max-w-(--breakpoint-xl) md:max-w-(--breakpoint-md) px-4">
                     <div className="flex md:flex-row flex-col items-center justify-center md:gap-28 gap-8 mt-10">
-                        Headline : We bridge the gap between business and AI.
+                        <h1>Headline : We bridge the gap between business and AI.</h1>
                         Headlines: We build custom AI chatbots and voice agents for Australian businesses.
                         Subheadings:Automate your customer service, capture more leads, and free up your team’s time — all powered by Australian-built AI that speaks your language.
                         More Text : Founded by IT project professionals passionate about innovation, our mission is to help organisations embrace AI confidently and responsibly. With expertise across IT project delivery, data-driven strategy, and emerging technology, we partner with businesses to identify real opportunities — not hype. (Keep about us page thats final)
